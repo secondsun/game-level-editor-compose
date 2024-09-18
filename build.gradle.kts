b25de2d990f1fc2b500b2622c06b3eeb32f3c935
Summers Pittman <mrsummers@google.com>
2024-09-18 14:13:57 -0700

    Removing bitrot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    id("org.jetbrains.compose") version "1.5.0"
+    kotlin("jvm") version "1.9.24"
+    id("org.jetbrains.compose") version "1.6.11"
     id("com.diffplug.spotless") version "6.21.0"
 }
 
